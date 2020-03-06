8205e7c4ed1d00940c130053ba6ce8b66e7cf109
jjcc201-2 <josh.chalcraft@outlook.com>
2020-03-06 03:02:48 +0000

    fully set up outbox

--- outbox.go
+++ outbox.go
@@ -16,18 +16,11 @@ type Email struct { // Type is used to refer to the struct afterwards
 	Message string
 }
 
-//type Emails []Email
-
 type Emails map[string]Email
 
 var emails map[string]Email // ONE EMAIL PER USER
 
 var outbox map[string]Emails
-
-/*
-1. Add email to user's outbox
-2. List emails
-*/
 
 // List emails from a user's outbox
 func List(w http.ResponseWriter, r *http.Request) {
@@ -53,24 +46,8 @@ func List(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-/*
 // Create email and add it to the user's outbox
 func Create(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	user := vars["user"]
-	decoder := json.NewDecoder(r.Body)
-	var email Email
-
-	if err := decoder.Decode(&email); err == nil { //If no errors in decoding the message into the email object
-		w.WriteHeader(http.StatusCreated)
-		outbox[user] = append(outbox[user], email)
-	} else {
-		w.WriteHeader(http.StatusBadRequest) // If there is an error with the JSON, send back a bad status request
-	}
-}
-*/
-// Create email and add it to the user's outbox
-func Create2(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	user := vars["user"]
 	decoder := json.NewDecoder(r.Body)
@@ -91,19 +68,62 @@ func Create2(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusBadRequest) // If there is an error with the JSON, send back a bad status request
 		}
 	} else {
-		// For when we cannot make the bucket
+		// For when we cannot make the email
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
 // Read a particular email from the outbox
 func Read(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := vars["user"]
+	uuid := vars["uuid"]
+
+	// If the user in question does exist
+	if _, ok := outbox[user]; ok {
+
+		// If the email in question does exist
+		if _, ok := outbox[user][uuid]; ok {
+			email := outbox[user][uuid]
+			if enc, err := json.Marshal(email); err == nil { // If you have an error converting it to JSON
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(enc)) // Write the output onto the response
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		} else {
+			// Cannot find the email
+			w.WriteHeader(http.StatusNotFound)
+		}
+	} else {
+		// Cannot find the user
+		w.WriteHeader(http.StatusNotFound)
+	}
 
 }
 
 // Delete a particular email from the outbox
 func Delete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := vars["user"]
+	uuid := vars["uuid"]
 
+	// If the user in question does exist
+	if _, ok := outbox[user]; ok {
+
+		// If the email in question does exist
+		if _, ok := outbox[user][uuid]; ok {
+			w.WriteHeader(http.StatusNoContent) // It all worked, but I have nothing else to say
+			delete(outbox[user], uuid)
+
+		} else {
+			// Cannot find the email
+			w.WriteHeader(http.StatusNotFound)
+		}
+	} else {
+		// Cannot find the user
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func handleRequests() {
@@ -111,7 +131,7 @@ func handleRequests() {
 	router.HandleFunc("/outbox/{user}/{uuid}", Delete).Methods("DELETE")
 	router.HandleFunc("/outbox/{user}/{uuid}", Read).Methods("GET")
 	router.HandleFunc("/outbox/{user}", List).Methods("GET")
-	router.HandleFunc("/outbox/{user}", Create2).Methods("POST")
+	router.HandleFunc("/outbox/{user}", Create).Methods("POST")
 	log.Fatal(http.ListenAndServe(":4000", router))
 }
 
